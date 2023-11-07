9a56a661d5025163d71f04f440df8fd6ff4f2d91
Hugo Landau <hlandau@openssl.org>
2023-11-07 07:37:55 +0000

    Fix shebang lines for NixOS compatibility
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/tools/pull/171)

--- release-tools/release-aux/test_suite.sh
+++ release-tools/release-aux/test_suite.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # Copyright 2023 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
--- release-tools/stage-release.sh
+++ release-tools/stage-release.sh
@@ -1,10 +1,12 @@
-#! /bin/bash -e
+#! /usr/bin/env bash
 # Copyright 2020-2023 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
 # this file except in compliance with the License.  You can obtain a copy
 # in the file LICENSE in the source distribution or at
 # https://www.openssl.org/source/license.html
+
+set -e
 
 # This is the most shell agnostic way to specify that POSIX rules.
 POSIXLY_CORRECT=1
